0a5ae2e212b0db352dac35e7951b9596f59365f4
unknown <jzarz@JZARZ-VM.mare.esailors.net>
2013-11-19 18:14:15 +0100

    initial skeleton of project using gradle and scala with scala-test

---
+++ src/test/scala/MyTest.scala
@@ -0,0 +1,17 @@
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class MyTest extends FunSuite {
+
+  test("An empty Set should have size 0") {
+    assert(Set.empty.size == 0)
+  }
+
+  test("Invoking head on an empty Set should produce NoSuchElementException") {
+    intercept[NoSuchElementException] {
+      Set.empty.head
+    }
+  }
+}
